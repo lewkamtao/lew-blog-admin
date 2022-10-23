00c0b8897a7ac89d648ca64b90eb44d9d04158cd
lew <68851669+lewkamtao@users.noreply.github.com>
2022-10-23 16:57:39 +0800

    fix: set token

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,47 +1,6 @@
 import axios from 'axios';
 import { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 
-// 获取 url 参数
-const getToken = () => {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (pair[0] == 'token') {
-            delToken();
-            const token = pair[1].replace(/%20/g, ' ');
-            localStorage.setItem('token', token);
-            return;
-        }
-    }
-
-    return false;
-};
-// 去除URL指定参数
-function delToken() {
-    var url = window.location.href; //页面url
-    var urlParam = window.location.search.substr(1); //页面参数
-    var beforeUrl = url.substr(0, url.indexOf('?')); //页面主地址（参数之前地址）
-    var nextUrl = '';
-
-    var arr = new Array();
-    if (urlParam != '') {
-        var urlParamArr = urlParam.split('&'); //将参数按照&符分成数组
-        for (var i = 0; i < urlParamArr.length; i++) {
-            var paramArr = urlParamArr[i].split('='); //将参数键，值拆开
-            //如果键雨要删除的不一致，则加入到参数中
-            if (paramArr[0] != 'token') {
-                arr.push(urlParamArr[i]);
-            }
-        }
-    }
-    if (arr.length > 0) {
-        nextUrl = '?' + arr.join('&');
-    }
-    url = beforeUrl + nextUrl;
-    history.pushState('', '', url); // 不刷新页面
-}
-
 //将axios封装到类中
 class xwlRequest {
     instance: AxiosInstance; //axios的实例将被保存到这里
@@ -52,8 +11,7 @@ class xwlRequest {
             //实例中的请求拦截器
             (config: AxiosRequestConfig) => {
                 //请求成功的拦截
-                const token = getToken() || localStorage.getItem('token');
-
+                const token = localStorage.getItem('token');
                 if (token) {
                     config.headers = {
                         Authorization: `${token}`
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,33 @@ import './styles/index.scss';
 import { VMdEditor } from '../src/utils/v-markdown';
 
 // 创建vue实例
+
+// 获取 url 参数
+const setToken = () => {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (pair[0] == 'token') {
+            delToken();
+            const token = pair[1].replace(/%20/g, ' ');
+            localStorage.setItem('token', token);
+            return;
+        }
+    }
+    return false;
+};
+
+// 去除token
+const delToken = () => {
+    var url =
+        window.location.protocol + '//' + window.location.host + '' + window.location.pathname;
+    window.location.replace(url); // 不刷新页面
+};
+
+// 设置token
+
+setToken();
 const app = createApp(App);
 
 app.use(Lew);
